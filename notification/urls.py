c6f61d25915acb3d947acf2a61effa5b41f4641f
sin <xuanmingyi@gmail.com>
2017-08-22 14:30:27 +0800

    add health page

--- notification/urls.py
+++ notification/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 
-from notification.views import index
+from notification.views import index, health
 
 urlpatterns = [
+    url(r'^health$', health),
     url(r'^$', index),
 ]
--- notification/views.py
+++ notification/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("dafasf")
+
+def health(request):
+    return HttpResponse("7")
